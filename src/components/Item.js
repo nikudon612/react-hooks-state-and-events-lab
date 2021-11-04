de5a74d27453a3d4d57d169c165b7c1c555d351a
Nick Bechtel <bechtel.nick@gmail.com>
2021-11-03 23:50:38 -0400

    commit tree 945eb7309192750de3ec6240e375ee346168c984

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setisInCart] = useState(false);
+
+  function handleAddedToCartClick() {
+    setisInCart((isInCart) => !isInCart);
+  }
   return (
-    <li className="">
+    <li className={isInCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button
+        className={isInCart ? "remove" : "add"}
+        onClick={handleAddedToCartClick}
+      >
+        {isInCart ? "Remove From" : "Add to"} Cart
+      </button>
     </li>
   );
 }
